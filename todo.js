555b582bdab84ed8970aec686a47b7205fc15cc0
feb-march-2019-bootcamp <engsoft77@gmail.com>
2019-03-02 22:08:29 +0300

    week6_project6

---
+++ todo.js
@@ -0,0 +1,11 @@
+import Vue from 'vue/dist/vue'
+import Todo from './todo.vue'
+
+Vue.component('todo', Todo)
+
+new Vue({
+    el: '#app',
+});
+
+
+
---
+++ todo.vue
@@ -0,0 +1,71 @@
+<template>
+<div>
+      <div>
+            <ul >
+                <li v-for="service in services" @click="ActiveLi(service)" :class="{ 'active': service.active}">
+              {{service.name}} 
+              <span></br>{{service.price  | currency}}</span> </li>
+              </br>
+			  </br>
+			  </br>
+            <div class="total"> </br> Total</br> {{total() | currency }} </div>
+        </div>
+         
+        </div>
+</template>
+<script>
+export default {
+    
+    data:()=>({
+    	
+        services: [
+        	{
+        		name: 'Web Development',
+        		price: 200,
+        		active:false
+        	},{
+        		name: 'Design',
+        		price: 450,
+        		active:false
+        	},{
+        		name: 'Integration',
+        		price: 150,
+        		active:false
+        	},{
+        		name: 'Training',
+        		price: 300,
+        		active:false
+        	}
+        ]
+	}),
+	filters:{
+		currency(c){
+			return '$'+ (c).toFixed(3);
+		}
+	},
+    methods: {
+    	ActiveLi(serv){
+            serv.active = !serv.active;
+    	},
+    	total(){
+
+        	 let total = 0.00;
+
+        	this.services.map(x=>{
+        		if (x.active){
+        			total=total+ x.price;
+        		}
+        	});
+
+    	   return total;
+        }
+    }
+};
+	
+
+</script>
+
+<style scoped>
+
+</style>
+
